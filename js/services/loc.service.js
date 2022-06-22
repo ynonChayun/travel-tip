2c9371826423c71998ebe8dc698b1982e0e90bb4
Maayan <millermaayan1.com>
2022-06-22 14:49:24 +0300

    add

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,18 @@
 export const locService = {
-    getLocs
+	getLocs,
 }
 
+import {storageService} from './storage.service.js'
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+	{name: 'Greatplace', lat: 32.047104, lng: 34.832384},
+	{name: 'Neveragain', lat: 32.047201, lng: 34.832581},
 ]
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(locs)
+		}, 2000)
+	})
 }
-
-
